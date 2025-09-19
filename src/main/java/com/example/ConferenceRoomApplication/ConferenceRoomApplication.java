e053907e3af0f91a95c31e19e0d73d1d81c084ae
vamsi9024 <petavamsikrishna9024@gmail.com>
2025-09-19 22:55:17 +0530

    commited

---
+++ src/main/java/com/example/ConferenceRoomApplication/ConferenceRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConferenceRoomApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceRoomApplication.class, args);
+	}
+
+}
